6901e02100e8728f39cc31c9975c16690361ac3d
Wilmer <wilmermartinez@wilmers-Laptop.local>
2024-01-08 14:33:18 -0800

    finished about card and theme button

--- js/script.js
+++ js/script.js
@@ -3,4 +3,29 @@
  * @author Sadee <codewithsadee24@gmail.com>
  */
 
-"use strict";
+'use strict';
+
+/**
+ * Light theme and dark mode
+ * */
+
+const /* {Node Element} */ $themeBtn =
+		document.querySelector('[data-theme-btn]');
+const /* {Node Element} */ $HTML = document.documentElement;
+let /* Boolean | String */ isDark = window.matchMedia(
+		'(prefers-color-scheme:dark)'
+	).matches;
+
+if (sessionStorage.getItem('theme')) {
+	$HTML.dataset.theme = sessionStorage.getItem('theme');
+} else {
+	$HTML.dataset.theme = isDark ? 'dark' : 'light';
+}
+
+const changeTheme = () => {
+	$HTML.dataset.theme =
+		sessionStorage.getItem('theme') === 'light' ? 'dark' : 'light';
+	sessionStorage.setItem('theme', $HTML.dataset.theme);
+};
+
+$themeBtn.addEventListener('click', changeTheme);
